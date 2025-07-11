9d057f4305330907ca963a5d2a45686826f0230b
IvaneHo <10608@holbertonstudents.com>
2025-07-11 19:42:38 +0200

    utilité de faire ça ??????????????

--- javascript-warm_up/2-arguments.js
+++
@@ -1,9 +0,0 @@
-const argc = process.argv.length - 2;
-
-if (argc === 0) {
-  console.log('No argument');
-} else if (argc === 1) {
-  console.log('Argument found');
-} else {
-  console.log('Arguments found');
-}
